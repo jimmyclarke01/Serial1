d75bcea0af18d7158f2978a656537bf0cc3baff6
James <james.clarke42@googlemail.com>
2016-04-04 19:28:58 +0100

    Initial Commit

---
+++ Serial1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Serial1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorts();
+        }
+
+        void getAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(comboBox1.Text=="" || comboBox2.Text=="")
+                {
+                    MessageBox.Show("Please Select Port Settings");
+                }
+                else
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.Open();
+                    progressBar1.Value = 100;
+                    textBox1.Enabled = true;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = false;
+                    button4.Enabled = true;
+                }
+            }
+            catch(UnauthorizedAccessException)
+            {
+                MessageBox.Show("Unauthorised Access");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            progressBar1.Value = 0;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            button4.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox2.Text = serialPort1.ReadLine();
+            }
+            catch(TimeoutException)
+            {
+                textBox2.Text = "Timeout Exception";
+            }
+        }
+    }
+}
